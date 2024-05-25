3aba5c7d6b755823581213d3052407ebfa33f6cc
Siwar112 <156667750+Siwar112@users.noreply.github.com>
2024-05-25 10:18:10 +0100

    Add files via upload

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Finale Version 16.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+    
+    
+];
